3deef8ccd1ad4b4fa09f9e71b4dca754c88541e2
Riya <riyathakurprof@gmail.com>
2024-04-21 23:53:34 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-practice</title>
+</head>
+<body>
+<h1>This is heading 1</h1>
+    <?php
+    $first_name = "Riya";
+    $last_name= "thakur";
+    echo "$first_name"; echo " "; echo "$last_name";
+
+
+    ?>
+    
+</body>
+</html>
