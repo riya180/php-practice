6a8f57fa4881804a61dac3699db4fadca52fbd86
Riya <riyathakurprof@gmail.com>
2024-04-23 20:55:22 +0530

    new change commit

---
+++ $and$$.php
@@ -0,0 +1,14 @@
+
+<?php 
+$x= 60;
+$y= "x";
+
+echo $$y;
+
+
+$name ="riya";
+$age = "name";
+ echo"<br>";
+ echo $$age;
+
+?>
---
+++ implode-explode.php
@@ -0,0 +1,15 @@
+
+<?php 
+$str = "i love my family";
+$arr = explode(" " , $str);
+echo "<br>";
+print_r($arr);
+
+
+$new_arr= ['i', 'love', 'php'];
+$new_str= implode(" ", $new_arr);
+echo "<br>";
+print_r($new_str)
+
+
+?>
